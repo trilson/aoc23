5a62fa67b2b82312a75318bbe719bbe8736908cb
Thomas Wilson <tom@trilson.co.uk>
2023-12-06 17:02:35 +0000

    my head hurts

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,16 +1,16 @@
-use std::{cmp::min, collections::HashMap};
+use std::cmp::{max, min};
 
 use crate::{utils::files::lines_from_file, Solution, SolutionPair};
 
+#[derive(Clone)]
 struct Range {
     destination_start: i64,
     source_start: i64,
     range_length: i64,
 }
 
-struct GardenMap {
-    map_from: String,
-    map_to: String,
+#[derive(Clone)]
+struct Block {
     ranges: Vec<Range>,
 }
 
@@ -18,16 +18,28 @@ pub fn solve() -> SolutionPair {
     let lines = lines_from_file("input/day05.txt");
     let first_line = lines[0].to_string();
 
-    let garden_map = generate_garden_map(lines);
+    let blocks = generate_blocks(lines);
+    let seeds_p1: Vec<i64> = get_seeds(&first_line);
 
-    let seeds_p1: Vec<i64> = generate_seeds_part1(&first_line);
-    let sol1: i64 = shortest_distance(seeds_p1, &garden_map);
-    let sol2: i64 = solve_part2(&garden_map, &first_line);
+    let sol1: i64 = shortest_distance(&seeds_p1, &blocks);
+
+    let seeds_p2: Vec<(i64, i64)> = get_seed_ranges(&seeds_p1);
+    let sol2: i64 = solve_part2(&blocks, &seeds_p2);
 
     (Solution::from(sol1), Solution::from(sol2))
 }
 
-fn generate_seeds_part1(line: &String) -> Vec<i64> {
+fn get_seed_ranges(seeds: &Vec<i64>) -> Vec<(i64, i64)> {
+    let mut ranges = Vec::new();
+    for pair in seeds.windows(2).step_by(2) {
+        if let [a, b] = pair {
+            ranges.push((a.to_owned(), a + b));
+        }
+    }
+    ranges
+}
+
+fn get_seeds(line: &String) -> Vec<i64> {
     let first_line = line.split(':').last();
 
     first_line
@@ -37,91 +49,86 @@ fn generate_seeds_part1(line: &String) -> Vec<i64> {
         .collect()
 }
 
-fn solve_part2(garden_map: &HashMap<String, GardenMap>, line: &String) -> i64 {
-    let first_line = line.split(':').last();
-    let seed_vector = Vec::from_iter(first_line.unwrap().split_whitespace());
-    let mut shortest_distance: i64 = i64::MAX;
-    for pair in seed_vector.windows(2).step_by(2) {
-        if let [a, b] = pair {
-            let orig_seed = a.parse::<i64>().unwrap();
-            let more_seeds = b.parse::<i64>().unwrap();
+fn solve_part2(blocks: &Vec<Block>, seed_pairs: &Vec<(i64, i64)>) -> i64 {
+    let mut seeds = seed_pairs.clone(); // Clone the initial seed pairs
+    let mut new_seeds;
+    let mut ranges;
 
-            for s in orig_seed..orig_seed + more_seeds {
-                shortest_distance = min(shortest_distance, distance_for_seed(garden_map, s));
+    for block in blocks {
+        new_seeds = Vec::new(); // Create a new vector for the next iteration
+        ranges = block.ranges.clone();
+        while let Some((s, e)) = seeds.pop() {
+            let mut no_seeds_pushed = true;
+            for range in &ranges {
+                let os = max(s, range.source_start);
+                let oe = min(e, range.source_start + range.range_length);
+
+                if os < oe {
+                    new_seeds.push((os - range.source_start + range.destination_start, oe - range.source_start + range.destination_start));
+                    if os > s {
+                        seeds.push((s, os));
+                    }
+                    if e > oe {
+                        seeds.push((oe, e));
+                    }
+                    no_seeds_pushed = false;
+                    break;
+                }
+            }
+            if no_seeds_pushed {                
+                new_seeds.push((s, e));
             }
         }
+
+        seeds = new_seeds; // Update seeds for the next block
     }
-    shortest_distance
+    println!("{:?}", seeds);
+    let min = seeds.iter().map(|(first, _)| first).min();
+    min.unwrap().to_owned()
 }
 
-fn generate_garden_map(lines: Vec<String>) -> HashMap<String, GardenMap> {
-    let mut garden_map: HashMap<String, GardenMap> = HashMap::new();
-    let mut current_gm: Option<String> = None;
+fn generate_blocks(lines: Vec<String>) -> Vec<Block> {
+    let mut blocks: Vec<Block> = Vec::new();
 
-    for el in 1..lines.len() - 1 {
-        if lines[el].contains(":") {
-            let current_map = String::from(lines[el].split(' ').next().unwrap());
-            let split_map: Vec<&str> = current_map.split('-').collect();
-            let source = split_map[0];
-
-            let gm = GardenMap {
-                map_from: source.to_owned(),
-                map_to: split_map[2].to_owned(),
-                ranges: Vec::new(),
-            };
-
-            garden_map.insert(source.to_owned(), gm);
-            current_gm = Some(source.to_owned());
-        } else if !lines[el].is_empty() {
-            let nums: Vec<&str> = lines[el].split(' ').collect();
-            if let Some(gm) = &mut current_gm {
-                let gm = garden_map.get_mut(gm).unwrap();
-                gm.ranges.push(Range {
+    for line in lines.iter().skip(1) {
+        if line.contains(":") {
+            // Start a new block when the line contains ":"
+            blocks.push(Block { ranges: Vec::new() });
+        } else if !line.is_empty() && !blocks.is_empty() {
+            // Split the line into numbers and add a range to the last block
+            let nums: Vec<&str> = line.split(' ').collect();
+            if let Some(last_block) = blocks.last_mut() {
+                last_block.ranges.push(Range {
                     destination_start: nums[0].parse::<i64>().unwrap(),
                     source_start: nums[1].parse::<i64>().unwrap(),
                     range_length: nums[2].parse::<i64>().unwrap(),
-                })
+                });
             }
         }
     }
-
-    for item in garden_map.values_mut() {
-        item.ranges.sort_by_key(|r| r.source_start);
-    }
-    garden_map
+    blocks
 }
 
-fn shortest_distance(seeds: Vec<i64>, garden_map: &HashMap<String, GardenMap>) -> i64 {
+fn shortest_distance(seeds: &Vec<i64>, blocks: &Vec<Block>) -> i64 {
     let mut shortest_distance: i64 = i64::MAX;
     for seed in seeds {
-        let dist = distance_for_seed(garden_map, seed);
+        let dist = distance_for_seed(&blocks, seed.to_owned());
         shortest_distance = min(shortest_distance, dist);
     }
     shortest_distance
 }
 
-fn distance_for_seed(garden_map: &HashMap<String, GardenMap>, seed: i64) -> i64 {
-    let mut gm = garden_map.get("seed");
+fn distance_for_seed(blocks: &Vec<Block>, seed: i64) -> i64 {
     let mut id = seed;
-    while gm.is_some() {
-        let cur_map = gm.unwrap();
+    for block in blocks {
         let mut new_id = id;
-        for r in &cur_map.ranges {
+        for r in &block.ranges {
             if id >= r.source_start && id < r.source_start + r.range_length {
                 new_id = (id - r.source_start) + r.destination_start;
                 break;
             }
-            if id < r.source_start {
-                break;
-            }
         }
         id = new_id;
-        gm = garden_map.get(&cur_map.map_to.to_string());
     }
     id
-}
-
-#[test]
-fn it_works() {
-    println!("{:?}", solve());
 }
