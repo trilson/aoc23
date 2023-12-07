d542f5af5cba47791b54a3998534bee6bc7cb43c
Thomas Wilson <tom@trilson.co.uk>
2023-12-07 14:59:17 +0000

    cleanup

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -12,18 +12,10 @@ struct Hand {
 
 pub fn solve() -> SolutionPair {
     let lines = lines_from_file("input/day07.txt");
-    (Solution::from(part1(&lines)), Solution::from(part2(&lines)))
-}
-
-fn part1(lines: &[String]) -> i32 {
-    let card_rank_pt1: HashMap<char, usize> = card_rank_pt1();
-    solve_part(lines, card_rank_pt1, false)
-}
-
-
-fn part2(lines: &[String]) -> i32 {
-    let card_rank_pt1: HashMap<char, usize> = card_rank_pt2();
-    solve_part(lines, card_rank_pt1, true)
+    (
+        Solution::from(solve_part(&lines, card_rank_pt1(), false)),
+        Solution::from(solve_part(&lines, card_rank_pt2(), true)),
+    )
 }
 
 fn solve_part(lines: &[String], card_rank_pt1: HashMap<char, usize>, j_wildcard: bool) -> i32 {
@@ -134,13 +126,14 @@ fn reassign_j(card_count: &mut HashMap<char, i32>) {
 }
 
 fn find_max_by_value_except_j(card_count: &HashMap<char, i32>) -> Option<char> {
-    card_count.iter()
+    card_count
+        .iter()
         .filter(|&(k, _v)| *k != 'J')
         .max_by(|a, b| a.1.cmp(&b.1))
         .map(|(k, _v)| *k)
 }
 
-fn assign_type(card_count: &HashMap<char, i32>) -> u8 {    
+fn assign_type(card_count: &HashMap<char, i32>) -> u8 {
     let count_set: HashSet<&i32> = card_count.values().collect();
     match card_count.len() {
         1 => 7,
