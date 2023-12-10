69f861be98568cdeb7927a9a31c13f69743e6fbc
Thomas Wilson <tom@trilson.co.uk>
2023-12-10 17:28:07 +0000

    day10 - pt2 fail

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,12 +1,111 @@
-use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use crate::{utils::files::lines_from_file, Solution, SolutionPair};
 
-///////////////////////////////////////////////////////////////////////////////
+#[derive(Debug)]
+struct PipeSection {
+    y_from: i32,
+    y_to: i32,
+    x_from: i32,
+    x_to: i32,
+}
+
+impl PipeSection {
+    fn new(y_from: i32, y_to: i32, x_from: i32, x_to: i32) -> PipeSection {
+        PipeSection {
+            y_from,
+            y_to,
+            x_from,
+            x_to,
+        }
+    }
+
+    fn from_char(value: char) -> Option<Self> {
+        match value {
+            '|' => Some(PipeSection::new(-1, 1, 0, 0)),
+            '-' => Some(PipeSection::new(0, 0, -1, 1)),
+            'L' => Some(PipeSection::new(-1, 0, 0, 1)),
+            'J' => Some(PipeSection::new(0, -1, -1, 0)),
+            '7' => Some(PipeSection::new(1, 0, 0, -1)),
+            'F' => Some(PipeSection::new(1, 0, 0, 1)),
+            'S' => Some(PipeSection::new(0, 0, 0, 0)),
+            _ => None,
+        }
+    }
+}
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
+    let lines = lines_from_file("input/day10.txt");
+    let (grid, start) = parse_grid(&lines);
+
+    let mut previous = start;
+    let mut current = get_pipe_start(&grid, (start.0, start.1));
+
+    let mut steps = 0;
+    while current != start {
+        if let Some(pipe) = get_pipe_element(&grid, current) {
+            let (x_offset, y_offset) =
+                if previous == (pipe.x_from + current.0, pipe.y_from + current.1) {
+                    (pipe.x_to, pipe.y_to)
+                } else {
+                    (pipe.x_from, pipe.y_from)
+                };
+
+            previous = current;
+            current = (x_offset + current.0, y_offset + current.1);
+            steps += 1;
+        }
+    }
+    let sol1: u64 = (steps + 1) / 2;
     let sol2: u64 = 0;
 
+    // Next... double grid resolution and perform a flood-fill. Count the remainder
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn get_pipe_element(grid: &Vec<Vec<Option<PipeSection>>>, x_y: (i32, i32)) -> &Option<PipeSection> {
+    let x = grid.get(x_y.0 as usize);
+    if let Some(x_vec) = x {
+        if let Some(y) = x_vec.get(x_y.1 as usize) {
+            return y;
+        }
+    }
+    &None
+}
+
+fn get_pipe_start(grid: &Vec<Vec<Option<PipeSection>>>, start: (i32, i32)) -> (i32, i32) {
+    for tile in [(0, -1), (-1, 0), (1, 0), (0, 1)] {
+        let x_try = start.0 + tile.0;
+        let y_try = start.1 + tile.1;
+        if x_try < 0 || y_try < 0 {
+            continue;
+        }
+        if let Some(x) = grid.get(x_try as usize) {
+            if let Some(section) = x.get(y_try as usize) {
+                if let Some(pipe) = section {
+                    if pipe.x_from == tile.0 as i32 || pipe.y_from == tile.1 as i32 {
+                        return (x_try, y_try);
+                    }
+                }
+            }
+        }
+    }
+    (0, 0)
+}
+
+fn parse_grid(lines: &Vec<String>) -> (Vec<Vec<Option<PipeSection>>>, (i32, i32)) {
+    let mut map: Vec<Vec<Option<PipeSection>>> = Vec::new();
+    let mut start = (0, 0);
+    for (idy, line) in lines.iter().enumerate() {
+        for (idx, char) in line.chars().enumerate() {
+            let section = PipeSection::from_char(char);
+            if let Some(x) = map.get_mut(idx) {
+                x.push(section);
+            } else {
+                map.push(vec![section]);
+            }
+            if char == 'S' {
+                start = (idx as i32, idy as i32);
+            }
+        }
+    }
+    (map, start)
 }
