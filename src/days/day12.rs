494088674284572282d6dfdc01c9dcdd3cfe040d
Thomas Wilson <tom@trilson.co.uk>
2023-12-12 22:04:37 +0000

    slightly quicker

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -14,50 +14,58 @@ fn solve_recursive(lines: &Vec<String>, multiple: usize) -> i64 {
     let mut sol = 0;
     for line in lines {
         let parts = line.split_whitespace().collect::<Vec<_>>();
-        let springs = repeat(parts[0]).take(multiple).collect::<Vec<&str>>().join("?");
-        let damaged = repeat(parts[1]).take(multiple).collect::<Vec<&str>>().join(",");
+        let springs = repeat(parts[0])
+            .take(multiple)
+            .collect::<Vec<&str>>()
+            .join("?");
+        let damaged = repeat(parts[1])
+            .take(multiple)
+            .collect::<Vec<&str>>()
+            .join(",");
 
         let contiguous_parts: Vec<i32> = damaged
             .split(",")
             .filter_map(|s| s.parse::<i32>().ok())
             .collect();
 
-        let mut memo: HashMap<(String, i32), i64> = HashMap::new();
-        sol += count(&springs, &contiguous_parts, &mut memo);
+        let mut memo: HashMap<(&[u8], usize), i64> = HashMap::new();
+        sol += count(springs.as_bytes(), &contiguous_parts, &mut memo);
     }
     sol
 }
 
-fn count(line: &str, parts: &[i32], memo: &mut HashMap<(String, i32), i64>) -> i64 {
+fn count<'a>(line: &'a [u8], parts: &[i32], memo: &mut HashMap<(&'a [u8], usize), i64>) -> i64 {
     if line.is_empty() {
         return if parts.is_empty() { 1 } else { 0 };
     }
 
     if parts.is_empty() {
-        return if line.contains('#') { 0 } else { 1 };
+        return if line.contains(&b'#') { 0 } else { 1 };
     }
 
-    let memo_key = (line.to_string(), parts.len() as i32);
+    let memo_key = (line, parts.len());
     if let Some(&cached_result) = memo.get(&memo_key) {
         return cached_result;
     }
 
-    let mut result: i64 = 0;
-    let first_char = line.as_bytes()[0];
+    let mut result = 0;
+    let first_char = line[0];
 
     if first_char == b'.' || first_char == b'?' {
         result += count(&line[1..], parts, memo);
     }
 
     if first_char == b'#' || first_char == b'?' {
-        if let Some(&part_size) = parts.get(0) {
-            let can_place_part = part_size as usize <= line.len()
-                && !line[..part_size as usize].contains('.')
-                && (part_size as usize == line.len()
-                    || line.as_bytes()[part_size as usize] != b'#');
-
-            if can_place_part {
-                let remainder = line.get((part_size as usize + 1)..).unwrap_or("");
+        if let Some(&part_size) = parts.first() {
+            if part_size as usize <= line.len()
+                && !line[..part_size as usize].contains(&b'.')
+                && (part_size as usize == line.len() || line[part_size as usize] != b'#')
+            {
+                let remainder: &[u8] = if part_size < line.len() as i32 {
+                    &line[(part_size as usize) + 1..]
+                } else {
+                    &[]
+                };
                 result += count(remainder, &parts[1..], memo);
             }
         }
@@ -65,13 +73,4 @@ fn count(line: &str, parts: &[i32], memo: &mut HashMap<(String, i32), i64>) -> i
 
     memo.insert(memo_key, result);
     result
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = super::solve();
-        println!("{result:?}");
-    }
 }
