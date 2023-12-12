a8f5bc813c29d878946b3bd4454c787b3838bd6a
Thomas Wilson <tom@trilson.co.uk>
2023-12-12 21:42:50 +0000

    help me i'm recursing

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,68 +1,70 @@
-use std::iter::repeat;
+use std::{collections::HashMap, iter::repeat};
 
-use itertools::Itertools;
 use crate::{utils::files::lines_from_file, Solution, SolutionPair};
 
 pub fn solve() -> SolutionPair {
     let lines = lines_from_file("input/day12.txt");
+    let sol1 = solve_recursive(&lines, 1);
+    let sol2 = solve_recursive(&lines, 5);
 
-    let mut poss = 0;
+    (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn solve_recursive(lines: &Vec<String>, multiple: usize) -> i64 {
+    let mut sol = 0;
     for line in lines {
         let parts = line.split_whitespace().collect::<Vec<_>>();
-        let springs = parts[0]; // repeat(parts[0]).take(5).collect::<Vec<&str>>().join("?");
-        let damaged = parts[1]; // repeat(parts[1]).take(5).collect::<Vec<&str>>().join(",");
+        let springs = repeat(parts[0]).take(multiple).collect::<Vec<&str>>().join("?");
+        let damaged = repeat(parts[1]).take(multiple).collect::<Vec<&str>>().join(",");
 
-        let answers: Vec<i32> = springs
-            .chars()
-            .enumerate()
-            .filter_map(|(idx, s)| {
-                if s == '?' {
-                    return Some(idx as i32);
-                } else {
-                    return None;
-                }
-            }).collect();
-
-        let cont: Vec<i32> = damaged
+        let contiguous_parts: Vec<i32> = damaged
             .split(",")
             .filter_map(|s| s.parse::<i32>().ok())
             .collect();
 
-        let questions: i32 =
-            cont.iter().sum::<i32>() - line.chars().filter(|&c| c == '#').count() as i32;
-
-        for comb in answers
-            .into_iter()
-            .combinations(questions as usize) {
-                let mut contiguous = 0;
-                let mut value_check = 0_i32;
-                for (idx, ch) in parts[0].chars().enumerate() {
-                    if ch == '#' || comb.contains(&(idx as i32)) {
-                        contiguous += 1;
-                    } else if contiguous != 0 {
-                        let exp = cont.get(value_check as usize).unwrap_or(&-1);
-                        if exp != &contiguous {
-                            value_check = -1;
-                            break;
-                        } else {
-                            value_check += 1;
-                            contiguous = 0;
-                        }
-                    }
-                }
-                if value_check == -1 {
-                    continue;
-                }
-                let exp = cont.get(value_check as usize).unwrap_or(&-1);
-                if contiguous == 0 || exp == &contiguous {
-                    poss += 1;
-                }
-            }
-        // count number of #, subtract from expected number (1+1+3) = C
-        // collect co-ordinates of ? and apply combinations (C |comb| ?)
-        // On each combination, check 1,1,3 progression
+        let mut memo: HashMap<(String, Vec<i32>), i64> = HashMap::new();
+        sol += count(&springs, &contiguous_parts, &mut memo);
     }
-    (Solution::from(poss), Solution::from(0))
+    sol
+}
+
+fn count(line: &str, parts: &[i32], memo: &mut HashMap<(String, Vec<i32>), i64>) -> i64 {
+    if line.is_empty() {
+        return if parts.is_empty() { 1 } else { 0 };
+    }
+
+    if parts.is_empty() {
+        return if line.contains('#') { 0 } else { 1 };
+    }
+
+    let memo_key = (line.to_string(), parts.to_vec());
+    if let Some(&cached_result) = memo.get(&memo_key) {
+        return cached_result;
+    }
+
+    let mut result: i64 = 0;
+    let first_char = line.as_bytes()[0];
+
+    if first_char == b'.' || first_char == b'?' {
+        result += count(&line[1..], parts, memo);
+    }
+
+    if first_char == b'#' || first_char == b'?' {
+        if let Some(&part_size) = parts.get(0) {
+            let can_place_part = part_size as usize <= line.len()
+                && !line[..part_size as usize].contains('.')
+                && (part_size as usize == line.len()
+                    || line.as_bytes()[part_size as usize] != b'#');
+
+            if can_place_part {
+                let remainder = line.get((part_size as usize + 1)..).unwrap_or("");
+                result += count(remainder, &parts[1..], memo);
+            }
+        }
+    }
+
+    memo.insert(memo_key, result);
+    result
 }
 
 #[cfg(test)]
