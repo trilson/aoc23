7697e3eda340ab5031108a6bde48f8bb044e6ad6
Thomas Wilson <tom@trilson.co.uk>
2023-12-20 19:30:52 +0000

    ooph

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -1,4 +1,5 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{BTreeSet, HashMap, VecDeque};
+use num_integer::gcd;
 
 use crate::{utils::files::lines_from_file, Solution, SolutionPair};
 
@@ -21,12 +22,12 @@ struct Module {
     on: bool,
     module_type: ModuleType,
     targets: Vec<String>,
-    conj: HashSet<String>,
+    receivers: BTreeSet<String>,
 }
 
 impl Module {
     fn handle_source(&mut self, source: &str) {
-        self.conj.insert(source.to_owned());
+        self.receivers.insert(source.to_owned());
     }
 
     fn accept_beam(&mut self, incoming: BeamType, from: &str) -> Option<(BeamType, &Vec<String>)> {
@@ -45,11 +46,11 @@ impl Module {
         }
         if self.module_type == ModuleType::Conjunction {
             if incoming == BeamType::High {
-                self.conj.remove(from);
+                self.receivers.remove(from);
             } else {
-                self.conj.insert(from.to_owned());
+                self.receivers.insert(from.to_owned());
             }
-            if self.conj.is_empty() {
+            if self.receivers.is_empty() {
                 return Some((BeamType::Low, &self.targets));
             } else {
                 return Some((BeamType::High, &self.targets));
@@ -58,8 +59,17 @@ impl Module {
         None
     }
 }
+
 pub fn solve() -> SolutionPair {
     let lines = lines_from_file("input/day20.txt");
+    let modules = create_modules(lines);
+    (
+        Solution::from(solve_pt1(&mut modules.clone())),
+        Solution::from(solve_pt2(&mut modules.clone())),
+    )
+}
+
+fn create_modules(lines: Vec<String>) -> HashMap<String, Module> {
     let mut modules: HashMap<_, _> = lines
         .iter()
         .map(|m| {
@@ -72,26 +82,37 @@ pub fn solve() -> SolutionPair {
                     on: false,
                     module_type: ModuleType::FlipFlop,
                     targets,
-                    conj: HashSet::new(),
+                    receivers: BTreeSet::new(),
                 },
                 '&' => Module {
                     name: sp[0][1..].to_owned(),
                     on: true,
                     module_type: ModuleType::Conjunction,
                     targets,
-                    conj: HashSet::new(),
+                    receivers: BTreeSet::new(),
                 },
                 _ => Module {
                     name: sp[0].to_owned(),
                     on: true,
                     module_type: ModuleType::Broadcaster,
                     targets,
-                    conj: HashSet::new(),
+                    receivers: BTreeSet::new(),
                 },
             }
         })
         .map(|s| (s.name.to_owned(), s.clone()))
         .collect();
+
+    modules.insert(
+        "rx".to_owned(),
+        Module {
+            name: "rx".to_owned(),
+            on: false,
+            module_type: ModuleType::Conjunction,
+            targets: Vec::new(),
+            receivers: BTreeSet::new(),
+        },
+    );
 
     let updates: Vec<(String, Vec<String>)> = modules
         .values()
@@ -105,20 +126,21 @@ pub fn solve() -> SolutionPair {
             }
         }
     }
-    let sol1 = solve_pt1(&mut modules);
-    let sol2 = solve_pt2(&mut modules);
-    (Solution::from(sol1), Solution::from(sol2))
+    modules
 }
 
-fn solve_pt2(modules: &mut HashMap<String, Module>) -> i64 {
-    0
+fn lcm(a: i64, b: i64) -> i64 {
+    (a * b).abs() / gcd(a, b)
 }
 
+fn lcm_of_list(numbers: Vec<i64>) -> i64 {
+    numbers.iter().cloned().reduce(lcm).unwrap_or(1)
+}
 
 fn solve_pt1(modules: &mut HashMap<String, Module>) -> i64 {
-    let mut low =0;
+    let mut low = 0;
     let mut high = 0;
-    
+
     for _i in 0..1000 {
         let mut state = VecDeque::new();
         state.push_back((
@@ -147,8 +169,33 @@ fn solve_pt1(modules: &mut HashMap<String, Module>) -> i64 {
     low * high
 }
 
-#[test]
-fn run_me() {
-    let s = solve();
-    print!("{s:?}");
+fn solve_pt2(modules: &mut HashMap<String, Module>) -> i64 {
+    let mut map = HashMap::new();
+
+    for i in 1..i64::MAX {
+        let mut state = VecDeque::new();
+        state.push_back((
+            String::from("button"),
+            String::from("broadcaster"),
+            BeamType::Low,
+        ));
+        while let Some((from, to, beam)) = state.pop_front() {
+            {
+                if to == "qn" && beam == BeamType::High {
+                    map.insert(from.to_owned(), i);
+                    if map.len() == 4 {
+                        return lcm_of_list(map.values().cloned().collect());
+                    }
+                }
+                if let Some(module) = modules.get_mut(&to) {
+                    if let Some((new_beam, targets)) = module.accept_beam(beam, &from) {
+                        for target in targets.iter().cloned() {
+                            state.push_back((to.clone(), target, new_beam.clone()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    0
 }
